e3d31f0d58d4dafbaf3c812f64ab929796bb4a90
owproduction <artem.kislyanskih@yandex.ru>
2025-09-12 13:16:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from service import FlightService
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from flight import Flight
 
 db_config={
@@ -19,6 +19,14 @@ app = FastAPI(
 async def root():
     return {"message":"Hello from FastAPI"}
 
+@app.get("/flights")
+async def get_flights():
+    try:
+        return service.get_all()
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Ошибка при получении полётов: {str(e)}")
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app,host="0.0.0.0", port=8080)
+
